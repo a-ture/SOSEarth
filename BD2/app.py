449bf00efe40416697515f75cedac34526c13274
a-ture <a.ture@studenti.unisa.it>
2024-05-19 22:33:16 +0200

    add first component

--- BD2/app.py
+++ BD2/app.py
@@ -1,12 +1,36 @@
 from flask_bootstrap import Bootstrap
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask("SOS Planet")
 bootstrap = Bootstrap(app)
 
+
 @app.route('/')
+@app.route('/index')
+@app.route('/home')
 def index():
     return render_template('index.html', bootstrap=bootstrap)
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/problems')
+def problems():
+    return render_template('problems_earth.html')
+
+
+@app.route('/help')
+def help():
+    return render_template('help_earth.html')
+
+
+@app.route('/project')
+def project():
+    return render_template('project_earth.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
