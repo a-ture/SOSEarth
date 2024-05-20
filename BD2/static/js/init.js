0d38c86be3079aaee40ddd3fbe179a51a04c0632
a-ture <a.ture@studenti.unisa.it>
2024-05-20 15:04:26 +0200

    create jumbotron in home/problmes

---
+++ BD2/static/js/init.js
@@ -0,0 +1,7 @@
+// Includi il tuo script di inizializzazione direttamente nel file HTML
+import {initMDB, Carousel} from "mdb-ui-kit";
+
+
+// Initialization for ES Users con gli argomenti recuperati
+initMDB({Ripple: rippleEnabled, Tab: tabEnabled, Carousel});
+
